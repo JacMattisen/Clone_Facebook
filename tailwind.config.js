511efbb3e8bb6eae200d8a6a165a98d4d06dab9f
JacMattisen <jjm_jac@yahoo.com.br>
2024-05-15 12:29:34 +0200

    clone_facebook

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      spacing: {
+        '30': '7.5rem',
+        '290px' : '290px',
+        '396px' : '396px',
+        '500' : '500px',
+        '900' : '900px'
+      },
+      fontSize: {
+        'subtitle' : '1.7rem'
+      },
+      colors: {
+        'main-gray' : '#f0f2f5',
+        'main-blue' : '#1877f2',
+        'main-blue-hover' : '#166fe5',
+        'main-green' : '#42872A',
+        'main-green-hover' : '#36A420',
+      }
+    },
+  },
+  plugins: [],
+}
+
