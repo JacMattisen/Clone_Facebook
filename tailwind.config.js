1a01b1d2d4e248fe043d6e7727f39211e02f2c67
JacMattisen <jjm_jac@yahoo.com.br>
2024-05-20 13:58:04 +0200

    corrigindo erros

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
         '30': '7.5rem',
         '290px' : '290px',
         '396px' : '396px',
-        '500' : '500px',
-        '900' : '900px'
+        '500px' : '500px',
+        '980px' : '980px'
       },
       fontSize: {
         'subtitle' : '1.7rem'
@@ -23,7 +23,7 @@ module.exports = {
         'main-gray' : '#f0f2f5',
         'main-blue' : '#1877f2',
         'main-blue-hover' : '#166fe5',
-        'main-green' : '#42872A',
+        'main-green' : '#42B72A',
         'main-green-hover' : '#36A420',
       }
     },
